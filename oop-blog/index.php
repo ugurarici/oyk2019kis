5ff56f1dc71678fea48e399292e516094160a9d2
Uğur Arıcı <m.ugurarici@gmail.com>
2019-02-11 16:40:37 +0300

    Delete legacy swtich-case code block

--- oop-blog/index.php
+++ oop-blog/index.php
@@ -1,35 +1,10 @@
 <?php
 
-//  index.php?op=list
-//  index.php?op=detail&id=5
-//  index.php?op=edit&id=5
-
 require_once "classes/Article.php";
 require_once "classes/ArticleController.php";
 
-switch ($_GET['op']) {
-    case 'list':
-        ArticleController::list();
-        break;
-    case 'create':
-        ArticleController::create();
-        break;
-    case 'store':
-        ArticleController::store();
-        break;
-    case 'delete':
-        ArticleController::delete();
-        break;
-    case 'detail':
-        ArticleController::detail();
-        break;
-    case 'edit':
-        ArticleController::edit();
-        break;
-    case 'update':
-        ArticleController::update();
-        break;
-    default:
-        header("Location: ?op=list");
-        break;
-}
+$requestedOperation = $_GET['op'];
+if(!method_exists('ArticleController', $requestedOperation))
+    $requestedOperation = "list";
+
+ArticleController::$requestedOperation();
