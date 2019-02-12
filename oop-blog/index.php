fb4543cdaf78f370df3b5e08dd1a42d87cce7b24
Uğur Arıcı <m.ugurarici@gmail.com>
2019-02-12 12:15:34 +0300

    Use composer's autoload to load our own classes

--- oop-blog/index.php
+++ oop-blog/index.php
@@ -1,8 +1,6 @@
 <?php
 
 require_once "vendor/autoload.php";
-require_once "classes/Article.php";
-require_once "classes/ArticleController.php";
 
 $requestedOperation = 'list';
 if(isset($_GET['op']) && method_exists('ArticleController', $_GET['op']))
